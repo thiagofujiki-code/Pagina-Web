d13da4b713b5752150a7dc2cfa5453dc7ddab64e
thiagofujiki-code <thiago.fujiki@escola.pr.gov.br>
2026-08-25 22:33:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const botoes = document.querySelectorAll('button');
 
 botoes.forEach(function(botao) {
+    let jaCurtiu = false;
+
     botao.addEventListener('click', function() {
         let texto = botao.querySelector('span');
+
         if (texto) {
             let numeroAtual = parseInt(texto.textContent) || 0;
-            texto.textContent = numeroAtual + 1;
+
+            if (jaCurtiu === false) {
+                texto.textContent = numeroAtual + 1;
+                jaCurtiu = true;
+            } else {
+                texto.textContent = numeroAtual - 1;
+                jaCurtiu = false;
+            }
         }
     });
 });
+
